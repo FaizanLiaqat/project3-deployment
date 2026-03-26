433f01cd639b79867cc14734e527b859556f41e5
FaizanLiaqat <faizanliaqat192@gmail.com>
2026-03-26 16:18:15 +0500

    config: update instance type and EC2 IP

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,7 +13,7 @@ variable "project_name" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.medium"
+  default     = "m7i-flex.large"
 }
 
 variable "ami_id" {
